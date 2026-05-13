069e00c9be81db8ffd131ce930b4805c348a82bc
shamsur-rahman-rifat <shamsur30rahman@gmail.com>
2026-05-13 11:16:01 +0600

    Vercel Config

--- src/server.js
+++ src/server.js
@@ -26,8 +26,10 @@ app.get('/', (req, res) => {
   res.send('UrbanWear API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+export default app;
